22575d37b15be0d465bdbbc5306fd8f4dca68513
saskanka <50367857+saskanka@users.noreply.github.com>
2022-08-23 08:52:44 -0400

    [YK][NODEMAILER] :add nodemailer - implement nodemailer test.js
    This reverts commit bfcd6f42a5bc2339aa872b0de53e1099411924c2.

---
+++ src/test.js
@@ -0,0 +1,30 @@
+const nodemailer = require("nodemailer");
+
+const email = { 
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: "e96b1930b9fd55",
+        pass: "c16cfe75196c9e"
+    }       
+}
+
+const send = (option) =>{
+    nodemailer.createTransport(email).sendMail(option, (error, info) =>{
+        if (error) {
+            console.log("*** error:", error)
+        } else {
+            console.log("*** info:", info)
+            return info.response;
+        }
+    })
+}
+
+const emailData = {
+    from: "saskanka@gmail.com",
+    to: "saskanka@gmail.com",
+    title: "test title",
+    text: "text test",
+}
+
+send(emailData)
