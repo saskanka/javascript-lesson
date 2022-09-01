8482959f8855a381e8da184542837f39b760738d
J K <jkang0724@hotmail.com>
2022-08-31 23:01:04 -0400

    [JK]

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,121 +1,165 @@
-/* old copy
-// e1
+// JavaScript Array Exercise 1
 
-const isEqualTo100 = (a, b) => a === 100 || b === 100 || a + b === 100;
+const people = [
+  {
+    firstName: "Sam",
+    lastName: "Hughes",
+    DOB: "07/07/1978",
+    department: "Development",
+    salary: "45000",
+  },
+  {
+    firstName: "Terri",
+    lastName: "Bishop",
+    DOB: "02/04/1989",
+    department: "Development",
+    salary: "35000",
+  },
+  {
+    firstName: "Jar",
+    lastName: "Burke",
+    DOB: "11/01/1985",
+    department: "Marketing",
+    salary: "38000",
+  },
+  {
+    firstName: "Julio",
+    lastName: "Miller",
+    DOB: "12/07/1991",
+    department: "Sales",
+    salary: "40000",
+  },
+  {
+    firstName: "Chester",
+    lastName: "Flores",
+    DOB: "03/15/1988",
+    department: "Development",
+    salary: "41000",
+  },
+  {
+    firstName: "Madison",
+    lastName: "Marshall",
+    DOB: "09/22/1980",
+    department: "Sales",
+    salary: "32000",
+  },
+  {
+    firstName: "Ava",
+    lastName: "Pena",
+    DOB: "11/02/1986",
+    department: "Development",
+    salary: "38000",
+  },
+  {
+    firstName: "Gabriella",
+    lastName: "Steward",
+    DOB: "08/26/1994",
+    department: "Marketing",
+    salary: "46000",
+  },
+  {
+    firstName: "Charles",
+    lastName: "Campbell",
+    DOB: "09/04/1977",
+    department: "Sales",
+    salary: "42000",
+  },
+  {
+    firstName: "Tiffany",
+    lastName: "Lambert",
+    DOB: "05/11/1990",
+    department: "Development",
+    salary: "34000",
+  },
+  {
+    firstName: "Antonio",
+    lastName: "Gonzalez",
+    DOB: "03/24/1985",
+    department: "Office Management",
+    salary: "49000",
+  },
+  {
+    firstName: "Aaron",
+    lastName: "Garrett",
+    DOB: "09/04/1985",
+    department: "Development",
+    salary: "39000",
+  },
+];
 
-console.log("equal to 100 function test");
+// Exercises
+/*
+// 1) What is the average income of all the people in the array?
+console.log(
+  people.reduce(
+    (accumulator, person) => accumulator + parseInt(person.salary),
+    0
+  ) / people.length
+);
 
-console.log(isEqualTo100(100, 0));
-console.log(isEqualTo100(0, 100));
+// 1-EX:  using function
+const averageIncome = (arr) =>
+  (
+    arr.reduce(
+      (accumulator, person) => accumulator + parseInt(person.salary),
+      0
+    ) / arr.length
+  ).toFixed(2);
 
-console.log(isEqualTo100(50, 50));
-console.log(isEqualTo100(49, 51));
+console.log(averageIncome(people));
 
-console.log(isEqualTo100(0, 0));
-console.log(isEqualTo100(100, 100));
+// 2) Who are the people that are currently older than 30?
+console.log(
+  people.filter(
+    (person) =>
+      new Date().getFullYear() - new Date(person.DOB).getFullYear() > 30
+  )
+);
 
-// e2
+// 2-EX:  30 years = 10957 days. ***WIP
+// const olderThan30 = (arr) =>
+//   arr.filter(
+//     (person) => new Date() - new Date(person.DOB) > 10957
+//   );
 
-const getFileExtension = (str) => str.slice(str.lastIndexOf("."));
+// console.log(olderThan30(people));
 
-console.log("get file extension test");
+// 3) Get a list of the people's full name (firstName and lastName).
+//console.log(people.map((person) => `${person.firstName} ${person.lastName}`));
+console.log(
+  people.map(({ firstName, lastName }) => `${firstName} ${lastName}`)
+);
 
-console.log(getFileExtension("index.html"));
-console.log(getFileExtension("webpack.config.js"));
-console.log(getFileExtension("noname"));
+// 3-EX:  Add full name property to array.
+const arrayWithFullname = (arr) =>
+  arr.map((person) => ({
+    ...person,
+    fullName: `${person.firstName} ${person.lastName}`,
+  }));
 
-// e3
+console.log(arrayWithFullname(people));
 
-// String.fromCharCode
-// charCodeAt
+4) Get a list of people in the array ordered from youngest to oldest.
+console.log(
+  people.sort(
+    (personA, personB) => new Date(personB.DOB) - new Date(personA.DOB)
+  )
+);
 
-const moveCharsForward = (str) =>
-  str
-    .split("")
-    .map((char) => String.fromCharCode(char.charCodeAt(0) + 1))
-    .join("");
+// 4-EX:  Ordered from oldest to youngest.
+const oldToYoung = (arr) =>
+  arr.sort((personA, personB) => new Date(personA.DOB) - new Date(personB.DOB));
 
-console.log("move alphabet to right test");
+console.log(oldToYoung(people));
 
-console.log(moveCharsForward("abcd"));
-console.log(moveCharsForward("z"));
-
-// e4
-
-const formatDate = (date = new Date()) => {
-  const days = date.getDate();
-  const months = date.getMonth() + 1;
-  const years = date.getFullYear();
-  return `${days}/${months}/${years}`;
-};
-
-console.log("show date test");
-
-console.log(formatDate());
-
-// e5
-
-const addNew = (str) => (str.indexOf("New!") === 0 ? str : `New! ${str}`);
-
-console.log("find New! and add New! test");
-
-console.log(addNew("New! Offers"));
-console.log(addNew("and Hot! Offers"));
-
-// e1
-
-const makeNewString = (str) =>
-  str.length < 3 ? str : str.slice(0, 3) + str.slice(-3);
-
-console.log("combine first and last 3 characters test");
-
-console.log(makeNewString("abc"));
-console.log(makeNewString("abcdef"));
-console.log(makeNewString("abc123def456"));
-
-console.log(makeNewString("ab"));
-
-// e2
-
-const firstHalf = (str) => str.slice(0, str.length / 2);
-
-console.log("get first half of the string test");
-
-console.log(firstHalf("temp"));
-console.log(firstHalf("temporary"));
-console.log(firstHalf("temporar"));
-
-// e3
-
-const concatenate = (str1, str2) => str1.slice(1) + str2.slice(1);
-
-console.log("concatenate two strings test");
-
-console.log(concatenate("abc", "def"));
-console.log(concatenate("Javascript", "isfun"));
-
-// e4
-
-const closestTo100 = (a, b) => (100 - a < 100 - b ? a : b);
-
-console.log("find value closer to 100 test");
-
-console.log(closestTo100(99, 1));
-console.log(closestTo100(50, 51));
-console.log(closestTo100(1, 1));
-
-// e5
-
-const countChars = (str, char) =>
-  str.split("").filter((ch) => ch === char).length;
-
-const contains2To4 = (str, char) =>
-  countChars(str, char) >= 2 && countChars(str, char) <= 4;
-
-console.log("find occurrencies test");
-
-console.log(contains2To4("google", "o"));
-console.log(contains2To4("goooogle", "o"));
-console.log(contains2To4("gogle", "o"));
-console.log(contains2To4("gooooogle", "o"));
+// 5) How many people are there in each department?
+console.log(
+  people.reduce(
+    (accumulator, person) => ({
+      ...accumulator,
+      [person.department]: accumulator[person.department] + 1 || 1,
+    }),
+    {}
+  )
+);
 */
